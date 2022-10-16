1eff3bf5dcd0fd0fd9007ff1455c06743fe41daf
Dhiraj singh <dhiru1126@gmail.com>
2022-10-16 23:53:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express =require("express")
+const app = express()
+
+const middleware1 = (req,res,next) =>{
+    console.log('middleware1')
+    next()
+}
+const middleware2 = (req,res,next) =>{
+    console.log('middleware2')
+    next()
+}
+
+app.use(middleware2)//using middleware2 globally
+
+app.get('/page1',middleware1,function (req,res) {
+    console.log('page1 - both middleware 1 and 2 applied')
+})//here we will get both middleware 1 and 2
+
+app.get('/page2',function (req,res) {
+    console.log('page2')
+})
+
+app.get('/page3',middleware1,function (req,res) {
+    console.log('page3 - - both middleware 1 and 2 applied')
+})
+
+app.get('/page4',function (req,res) {
+    console.log('page4')
+})
+
+app.get('/page5',function (req,res) {
+    console.log('page5')
+})
+
+app.listen(8080,function () {
+    console.log('App is running')
+})
